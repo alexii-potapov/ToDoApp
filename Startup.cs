cc4a3c88f00c89346113a5d3b3d74158b5dfa5e6
Aleksey Potapov <alexii.potapov@gmail.com>
2020-05-23 22:15:18 +0300

    Add MVC

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace ToDoApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(); //Добавляем сервис MVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,18 @@ namespace ToDoApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseRouting(); // Используем систему маршрутизации
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //    endpoints.MapGet("/", async context =>
+                //    {
+                //        await context.Response.WriteAsync("Hello World!");
+                //    });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
