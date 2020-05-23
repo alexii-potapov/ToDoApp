c0d7b08fd55c2a5404fd0b3687b67dcced2cbde2
Aleksey Potapov <alexii.potapov@gmail.com>
2020-05-24 00:24:36 +0300

    Add main classes

---
+++ Models/Todo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Duedate { get; set; }
+        public Fibonachi Complexity { get; set; }
+        public bool Archive { get; set; }
+        public bool Deleted { get; set; }
+        public Group Group { get; set; }
+
+    }
+
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public enum Fibonachi : ushort
+    {
+        P1 = 1,
+        P2 = 1,
+        P3 = 3,
+        P5 = 5,
+        P8 = 8,
+        P13 = 13,
+        P21 = 21,
+        P50 = 50,
+        P100 = 100
+    }
+}
